c2cc3f45a8126a624f5b268443fead9dcd7e00b6
gimz12 <kumalillankoon12@gmail.com>
2026-04-12 11:12:23 +0530

    Initial Commit

---
+++ AllerScan/AllerScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllerScanApp.swift
+//  AllerScan
+//
+//  Created by Kumal Illankoon on 2026-04-12.
+//
+
+import SwiftUI
+
+@main
+struct AllerScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
